d5323895d0a61ab73560c5048777484ec2c12632
JJoker0077 <1197779079@qq.com>
2018-01-20 04:57:55 +0800

    例子

---
+++ src/jicheng/Test2.java
@@ -0,0 +1,22 @@
+package jicheng;
+
+import danli.$missing$;
+import danli.Test2;
+
+public class Test2 {
+	/**
+	 * µ¥ÀıÄ£Ê½
+	 */
+	private static Test2 test2;
+
+private Test2  (){
+	
+	public static Test2 getInstence(){ 
+/*public static Test2 getIntance(){*/
+	if(test2==null){
+		test2=new Test2();
+	}
+}
+	return test2;
+}
+}
